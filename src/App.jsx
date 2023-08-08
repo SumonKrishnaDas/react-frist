1b10a4ee333bd20aa4236a75e714f5162f1e8faa
SumonKrishnaDas <sumondash51583@gmail.com>
2023-08-08 17:07:09 +0600

    added product

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Header from './components/Header'
+import  Shop from'./components/shop/Shop'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
 
 
 <Header>   </Header>
+<Shop> </Shop>
 
     </div>
   );
